19ff0ba864ccf7a7bafff718b43ada5bc316f8da
woynert <woynertgamer@gmail.com>
2023-10-25 17:02:30 -0500

    feat: follow user

---
+++ controller/friends.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"bitroll/codefest1-users/model"
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func (ctrl *Controller) FollowUser(req model.ReqFollowUser, userId uuid.UUID) error {
+
+	// write
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO followers (follower_user_uuid, followed_user_uuid)
+		VALUES ($1, $2)
+	`
+	_, err := ctrl.DB.ExecContext(
+		ctx,
+		query,
+		userId,
+		req.OtherUserId,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
