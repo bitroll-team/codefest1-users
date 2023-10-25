0ac0cee558f22b69c81cf1563d4de949717e0a0f
woynert <woynertgamer@gmail.com>
2023-10-25 14:51:15 -0500

    feat: challenge

--- sql/migrations/20231024223701_init.up.sql
+++ sql/migrations/20231024223701_init.up.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS users (
   "password_hash" VARCHAR NOT NULL,
   "role" ROLE NOT NULL
 );
+
+INSERT INTO users (uuid, username, email, full_name, password_hash, role)
+VALUES (
+  '0818f7a7-e8df-44c3-a981-52dd6150f2ab',
+  'theadmin',
+  'theadmin@mail.com',
+  'fulladmin',
+  '$2a$08$abHt86mhBpv7frwLLb//4ufNcIjhLzRfxXb6XPG1Qa54iyH4Sxw7W',
+  'ADMIN'
+)
